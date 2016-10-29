1645c0aa91ff30d0c2a167f77aff2b319f2cb68a
joylio <jiayu.dev@gmail.com>
2016-10-29 11:10:46 -0400

    fixed img format issues

--- js/gallery.js
+++ js/gallery.js
@@ -1,50 +1,50 @@
 var imgArr = {
 	"LeafVR | Jiayu Liu": [
-		"/images/leaf1.PNG",
-		"/images/leaf2.PNG",
-		"/images/leaf3.PNG",
-		"/images/leaf4.PNG"
+		"/images/leaf1.png",
+		"/images/leaf2.png",
+		"/images/leaf3.png",
+		"/images/leaf4.png"
 	],
 
 	"Color Run | Jiayu Liu": [
-		"/images/color1.PNG",
-		"/images/color2.PNG",
-		"/images/color3.PNG"
+		"/images/color1.png",
+		"/images/color2.png",
+		"/images/color3.png"
 	],
 
 	"Sound of Silence | Jiayu Liu": [
-		"/images/soundofsilence1.PNG"
+		"/images/soundofsilence1.png"
 	],
 
 	"Heat Man | Jiayu Liu": [
-		"/images/heatman1.PNG",
-		"/images/heatman2.PNG",
-		"/images/heatman3.PNG",
-		"/images/heatman4.PNG",
-		"/images/heatman5.PNG",
-		"/images/heatman6.PNG",
-		"/images/heatman7.PNG",
-		"/images/heatman8.PNG",
-		"/images/heatman9.PNG",
-		"/images/heatman10.PNG"
+		"/images/heatman1.png",
+		"/images/heatman2.png",
+		"/images/heatman3.png",
+		"/images/heatman4.png",
+		"/images/heatman5.png",
+		"/images/heatman6.png",
+		"/images/heatman7.png",
+		"/images/heatman8.png",
+		"/images/heatman9.png",
+		"/images/heatman10.png"
 	],
 
 	"Left Alive | Jiayu Liu": [
-		"/images/leftalive1.PNG"
+		"/images/leftalive1.png"
 	]
 }
 
 var count = 0;
-var title = imgArr[document.title];
+var image = imgArr[document.title];
 
 function ToggleImage() {
 	$(".gallery img").fadeOut(200, function() {
 		count++;
-		$(".gallery img").attr("src", title[count % title.length]).fadeIn(200);
+		$(".gallery img").attr("src", image[count % image.length]).fadeIn(200);
 	});
 }
 
 $(document).ready(function() {
-	if (title.length > 1)
+	if (image.length > 1)
 		setInterval('ToggleImage()', 3000);
 });
